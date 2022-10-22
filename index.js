f8f50147602ae8af9377024a6a14350260b8281f
YunshanJiang <yunshantree@gmail.com>
2022-10-23 03:38:21 +0800

    week7 done

---
+++ index.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+
+//parse JSON
+app.use(express.json());
+
+
+//db
+let dataStore = require('nedb');
+let db = new dataStore('week7.db');
+db.loadDatabase();
+app.use('/', express.static('public'));
+
+app.post('/postStudentINFO', (req, res)=>{
+   
+    db.insert(req.body,(err, newDocs)=>{
+        if(err) {
+            res.json({task: "task failed"});
+        } else {
+            res.json({task:"success"});
+        }
+
+    })
+
+});
+
+app.get('/getStudentINFO', (req, res)=>{
+    db.find({}, (err, docs)=> {
+        if(err) {
+            res.json({task: "task failed"})
+        } else {
+            let obj = {data: docs};
+            res.json(obj);
+        }
+
+    })
+
+});
+
+app.listen(3000, ()=>{
+    console.log(true);
+})
---
+++ public/app.js
@@ -0,0 +1,73 @@
+
+window.addEventListener(
+    'load',()=>{
+        
+       
+        getData();
+        
+        document.getElementById('submit').addEventListener('click', ()=>{
+            let fname = document.getElementById("fname").value;
+            let lname = document.getElementById("lname").value;
+            let ID = document.getElementById("ID").value;
+           
+
+            let obj = {
+                "fname":fname,
+                "lname":lname,
+                "ID":ID
+            }
+            let jsonData = JSON.stringify(obj);
+
+
+            fetch('/postStudentINFO',{
+                method: 'POST',
+                headers:{
+                    "Content-type":"application/json"
+                },
+                body:jsonData
+            })
+            .then(response=>response.json())
+            .then(data=>{console.log(data)})
+
+
+            getData();
+        })
+    }
+)
+
+
+function getData(){
+    document.getElementById('rightTerminal').innerHTML = "";
+    fetch('/getStudentINFO')
+    .then(response=>response.json())
+    .then(data=>{
+        for(let i=0;i<data.data.length;i++) {
+            let fname = data.data[i].fname;
+            let lname = data.data[i].lname;
+            let ID = data.data[i].ID;
+            let div = document.createElement('div');
+            div.classList.add("INFOGroup");
+            
+            let serialP = document.createElement('p');
+            serialP.innerHTML = i + 1;
+            serialP.classList.add("serialNumber");
+
+            let infoBlockDiv = document.createElement('div');
+            infoBlockDiv.classList.add("INFOBlock");
+
+            let fnameP = document.createElement('p');
+            fnameP.innerHTML = fname;
+            let lnameP = document.createElement('p');
+            lnameP.innerHTML = lname;
+            let IDP = document.createElement('p');
+            IDP.innerHTML = ID;
+            infoBlockDiv.appendChild(fnameP);
+            infoBlockDiv.appendChild(lnameP);
+            infoBlockDiv.appendChild(IDP);
+            div.appendChild(serialP);
+            div.appendChild(infoBlockDiv);
+            document.getElementById('rightTerminal').appendChild(div);
+        }
+        
+    })
+}
